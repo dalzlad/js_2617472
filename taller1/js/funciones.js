dd2053ca1363199f37bb4c0acd59acf88941490a
dalzlad <dalz.sena@gmail.com>
2023-07-27 11:25:09 -0500

    commit TALLER Act

--- taller1/js/funciones.js
+++ taller1/js/funciones.js
@@ -1,10 +1,12 @@
+
+
 /*
 Taller JS
-
 Emplear Sweet Alert, Boostrap o material o CSS puro o el framework css de sus gusto.
 
 1.Implementar un formulario en HTML que permita convertir
-una cantidad de pesos colombianos a euros y dólares americanos.
+una cantidad de pesos colombianos a euros y dólares 
+americanos.
 No se permiten cantidades negativas
 
 2.Consumir API: Implementar un formulario que permita buscar por 
@@ -14,5 +16,9 @@ La url de la API: https://www.datos.gov.co/resource/ceyp-9c7c.json
 
 3. Leer y mostrar el archivo aprendices.txt en una página html
 
-
 */
+
+//https://github.com/dalzlad/js_2617472
+
+//https://linuxhint.com/read-local-text-file-javascript/#:~:text=To%20read%20a%20locally%20placed,operation%3A%20reading%20a%20text%20file.
+//https://es.javascript.info/date
